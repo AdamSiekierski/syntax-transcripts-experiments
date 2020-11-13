0d9ad37eea1a5751e4c14f90e2cc3ec78cd9fa7c
Matt Huberty <matthewhuberty77@gmail.com>
2020-11-13 16:22:51 -0600

    Restructured files for multiple "main" packages. Added `talk_time` experiment

---
+++ experiments/talk_time/talk_time.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/AdamSiekierski/syntax-experiments/utils"
+)
+
+type talkTime struct {
+	wes   float64
+	scott float64
+}
+
+func getTotalTalkTime(shows utils.Transcripts) talkTime {
+	var tt talkTime
+
+	for _, show := range shows {
+		for _, sentence := range show {
+			talkSeconds := sentence.End - sentence.Start
+
+			talkMinutes := talkSeconds / 60
+
+			if sentence.Initials == "WB" {
+				tt.wes += talkMinutes
+			}
+
+			if sentence.Initials == "ST" {
+				tt.scott += talkMinutes
+			}
+		}
+	}
+
+	return tt
+}
+
+func main() {
+	transcripts, err := utils.LoadTranscripts()
+	if err != nil {
+		log.Fatal(fmt.Errorf("error loading transcript: %w", err))
+	}
+
+	totalTalkTime := getTotalTalkTime(transcripts)
+
+	fmt.Printf("Total talk time: %.2f minutes, %.2f hours.\n", totalTalkTime.scott+totalTalkTime.wes, (totalTalkTime.scott+totalTalkTime.wes)/60)
+	fmt.Printf("Total Wes talk time: %.2f minutes, %.2f hours.\n", totalTalkTime.wes, totalTalkTime.wes/60)
+	fmt.Printf("Total Scott talk time: %.2f minutes, %.2f hours.\n", totalTalkTime.scott, totalTalkTime.scott/60)
+}
